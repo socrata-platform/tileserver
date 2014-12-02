f5e4d2385fea6fbc1bcf01bced201ce5dea45a5e
Kaida Masaki <kaida.masaki@socrata.com>
2014-12-02 10:43:23 -0800

    Make TileServerConfig a singleton

--- src/main/scala/com/socrata/tileserver/config/TileServerConfig.scala
+++ src/main/scala/com/socrata/tileserver/config/TileServerConfig.scala
@@ -1,15 +1,13 @@
 package com.socrata.tileserver.config
 
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory}
 
 import com.socrata.thirdparty.curator.{CuratorConfig, DiscoveryConfig}
 
-class TileServerConfig(config: Config) {
+object TileServerConfig {
+  private val config = ConfigFactory.load().getConfig("com.socrata")
+
   val port = config.getInt("tileserver.port")
   val curator = new CuratorConfig(config, "curator")
   val discovery = new DiscoveryConfig(config, "curator")
-}
-
-object TileServerConfig {
-  def apply(config: Config): TileServerConfig = new TileServerConfig(config)
 }
