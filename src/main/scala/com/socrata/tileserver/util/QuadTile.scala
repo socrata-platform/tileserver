6b84c4b4e74066636fcbdb2194ca688d3d4ccc69
Kaida Masaki <kaida.masaki@socrata.com>
2014-12-02 17:09:48 -0800

    Fix off by one error with tile boundaries

--- src/main/scala/com/socrata/tileserver/util/QuadTile.scala
+++ src/main/scala/com/socrata/tileserver/util/QuadTile.scala
@@ -7,8 +7,8 @@ case class QuadTile(rawX: Int, rawY: Int, zoom: Int) {
   val (x: Int, y: Int) = mapper.tmsCoordinates(rawX, rawY)
 
   val north: Double = mapper.lat(y * Size)
-  val east:  Double = mapper.lon(x * Size + Size)
-  val south: Double = mapper.lat(y * Size + Size)
+  val east:  Double = mapper.lon(x * Size + Size - 1)
+  val south: Double = mapper.lat(y * Size + Size - 1)
   val west:  Double = mapper.lon(x * Size)
 
   def withinBox(pointColumn: String): String = {
