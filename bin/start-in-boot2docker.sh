5e8f2a8f011399d69c1506586fd97eaee0e2d538
Kaida Masaki <kaida.masaki@socrata.com>
2014-12-12 12:39:34 -0800

    Add curly braces to docker scripts

--- bin/start-in-boot2docker.sh
+++ bin/start-in-boot2docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DEV_ROOT="$HOME/Developer/Socrata/"
+PROJ_NAME='tileserver'
+PORT=2048
+DEV_ROOT="${HOME}/Developer/Socrata/"
 ENVFILE="/tmp/tileserver-env"     # This is the boot2docker vm path.
 
 ping -c 2 jenkins.sea1.socrata.com >/dev/null 2>/dev/null \
@@ -9,14 +11,14 @@ ping -c 2 jenkins.sea1.socrata.com >/dev/null 2>/dev/null \
 boot2docker init
 boot2docker down
 
-if [ ! -e "$DEV_ROOT/tileserver-docker/.git" ]; then
-    cd "$DEV_ROOT"
-    git clone --recursive git@git.socrata.com:tileserver-docker.git
+if [ ! -e "${DEV_ROOT}/${PROJ_NAME}-docker/.git" ]; then
+    cd "${DEV_ROOT}"
+    git clone --recursive git@git.socrata.com:${PROJ_NAME}-docker.git
 fi
 
 VBoxManage sharedfolder add 'boot2docker-vm' \
-    --name 'tileserver-docker' \
-    --hostpath "$DEV_ROOT/tileserver-docker" \
+    --name '${PROJ_NAME}-docker' \
+    --hostpath "${DEV_ROOT}/${PROJ_NAME}-docker" \
     --automount 2>/dev/null
 
 echo "Starting VM..."
@@ -24,29 +26,29 @@ $(boot2docker up 2>&1 | tail -n 4)
 
 ADDRS=$(ifconfig | grep 'inet ' | awk '{print $2}' | grep -v 127.0.0.1)
 
-if ! [ "$ENSEMBLE" ]; then
+if ! [ "${ENSEMBLE}" ]; then
     ENSEMBLE=$(
-        echo "$ADDRS" | while read addr; do
-            echo "$addr:2181"
+        echo "${ADDRS}" | while read addr; do
+            echo "${addr}:2181"
         done
     )
 
-    ENSEMBLE=$(echo $ENSEMBLE | sed 's/ /, /g')
+    ENSEMBLE=$(echo ${ENSEMBLE} | sed 's/ /, /g')
 fi
 
 boot2docker ssh <<EOF
-    mkdir tileserver-docker
-    sudo mount -t vboxsf -o uid=1000,gid=50 tileserver-docker tileserver-docker
-    cd tileserver-docker
-    docker build --no-cache --rm -t tileserver .
-    echo ZOOKEEPER_ENSEMBLE=[$ENSEMBLE] > $ENVFILE
-    sed -i 's/\[/["/' $ENVFILE
-    sed -i 's/, /", "/g' $ENVFILE
-    sed -i 's/]/"]/' $ENVFILE
-    echo starting tileserver
-    docker run --env-file=$ENVFILE -p 2048:2048 -d tileserver
+    mkdir ${PROJ_NAME}-docker
+    sudo mount -t vboxsf -o uid=1000,gid=50 ${PROJ_NAME}-docker ${PROJ_NAME}-docker
+    cd ${PROJ_NAME}-docker
+    docker build --no-cache --rm -t ${PROJ_NAME} .
+    echo ZOOKEEPER_ENSEMBLE=[${ENSEMBLE}] > ${ENVFILE}
+    sed -i 's/\[/["/' ${ENVFILE}
+    sed -i 's/, /", "/g' ${ENVFILE}
+    sed -i 's/]/"]/' ${ENVFILE}
+    echo starting ${PROJ_NAME}
+    docker run --env-file=${ENVFILE} -p ${PORT}:${PORT} -d ${PROJ_NAME}
 EOF
 
-echo 'Forwarding localhost:2048 to boot2docker (Ctrl+C to exit)'
-echo "boot2docker ssh -L 2048:localhost:2048 -N"
-boot2docker ssh -L 2048:localhost:2048 -N
+echo "Forwarding localhost:${PORT} to boot2docker (Ctrl+C to exit)"
+set -v
+boot2docker ssh -L ${PORT}:localhost:${PORT} -N
