ab891815058ef04b839f37b6c1148ee9199cfeb7
Kaida Masaki <kaida.masaki@socrata.com>
2015-01-27 16:16:27 -0800

    Revert "Allow setting loglevel via config"
    This reverts commit b0c1f0d5308e973a3b8ca9ffa71a20572d004b62.

--- src/main/scala/com.socrata.tileserver/tileserver/config/TileServerConfig.scala
+++ src/main/scala/com.socrata.tileserver/tileserver/config/TileServerConfig.scala
@@ -1,12 +1,6 @@
 package com.socrata.tileserver.config
 
-import scala.collection.JavaConverters._
-
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.classic.{Level, Logger}
-import ch.qos.logback.core.ConsoleAppender
 import com.typesafe.config.ConfigFactory
-import org.slf4j.LoggerFactory
 
 import com.socrata.thirdparty.curator.{CuratedClientConfig, DiscoveryBrokerConfig}
 
@@ -23,11 +17,5 @@ object TileServerConfig {
 
   /** Geo-Json Service Config. */
   lazy val upstream = new CuratedClientConfig(config, "upstream")
-
-  val logLevel = Level.toLevel(config.getString("logback.loglevel"), Level.INFO)
-
-  LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME) match {
-    case root: Logger => root.setLevel(logLevel)
-  }
 }
 // $COVERAGE-ON$
