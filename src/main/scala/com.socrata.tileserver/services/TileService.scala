42b4a8e943024ee9b55d1161b03b1b12020b12d2
Kaida Masaki <kaida.masaki@socrata.com>
2015-04-01 17:18:46 -0700

    Use parameterized logging

--- src/main/scala/com.socrata.tileserver/services/TileService.scala
+++ src/main/scala/com.socrata.tileserver/services/TileService.scala
@@ -78,11 +78,7 @@ case class TileService(client: CuratedServiceClient) extends SimpleResource {
     def createResponse(parsed: (JValue, Seq[FeatureJson])): HttpResponse = {
       val (jValue, features) = parsed
 
-      // $COVERAGE-OFF$
-      if (logger.isDebugEnabled) {
-        logger.debug(s"Underlying json: ${jValue.toString}")
-      }
-      // $COVERAGE-ON$
+      logger.debug(s"Underlying json: {}", jValue)
 
       val enc = TileEncoder(rollup(tile, features))
       val payload = ext match {
@@ -229,7 +225,7 @@ object TileService {
     }
 
     val pixels = maybePixels collect { case (Some(px), props) =>
-        (px, props)
+      (px, props)
     }
 
     val points = pixels groupBy { case (px, props) =>
