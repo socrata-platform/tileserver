a187212ff26a697cb83d24fcca81be184b4e9310
Kaida Masaki <kaida.masaki@socrata.com>
2015-07-15 14:24:32 -0700

    Fix style issues

--- src/main/scala/com.socrata.tileserver/services/TileService.scala
+++ src/main/scala/com.socrata.tileserver/services/TileService.scala
@@ -223,6 +223,7 @@ object TileService {
     }
   }
 
+  // scalastyle:off cyclomatic.complexity
   private[services] def soqlUnpackFeatures(rs: ResourceScope):
       Response => Try[(JValue, Iterator[FeatureJson])] = { resp: Response =>
     val dis = rs.open(new DataInputStream(resp.inputStream(Long.MaxValue)))
@@ -234,30 +235,30 @@ object TileService {
         Failure(InvalidSoqlPackException(soqlIter.headers))
       } else {
         val colNames = soqlIter.schema.map(_._1).toArray
-        val featureJsonIter = soqlIter.map { case soqlRow =>
+        val featureJsonIter = soqlIter.map { soqlRow =>
           val geom: Geometry = soqlRow(soqlIter.geomIndex) match {
             case SoQLMultiPolygon(mp) => mp
-            case SoQLPolygon(p)       => p
-            case SoQLPoint(pt)        => pt
-            case SoQLMultiPoint(mp)   => mp
-            case SoQLLine(l)          => l
-            case SoQLMultiLine(ml)    => ml
-            case x: SoQLValue         => throw new RuntimeException("Should not be seeing non-geom SoQL type" + x)
+            case SoQLPolygon(p) => p
+            case SoQLPoint(pt) => pt
+            case SoQLMultiPoint(mp) => mp
+            case SoQLLine(l) => l
+            case SoQLMultiLine(ml) => ml
+            case x: SoQLValue => throw new RuntimeException("Should not be seeing non-geom SoQL type" + x)
           }
           val props = (0 until soqlRow.size).filterNot(_ == soqlIter.geomIndex).map { i =>
             colNames(i) -> { soqlRow(i) match {
-                               case SoQLText(str)   => JString(str)
-                               case SoQLNumber(bd)  => JNumber(bd)
-                               case SoQLMoney(bd)   => JNumber(bd)
-                               case SoQLDouble(dbl) => JNumber(dbl)
-                               case SoQLBoolean(b)  => JBoolean(b)
-                               case SoQLFixedTimestamp(dt) => JString(SoQLFixedTimestamp.StringRep(dt))
-                               case SoQLFloatingTimestamp(dt) => JString(SoQLFloatingTimestamp.StringRep(dt))
-                               case SoQLTime(dt) => JString(SoQLTime.StringRep(dt))
-                               case SoQLDate(dt) => JString(SoQLDate.StringRep(dt))
-                               case other: SoQLValue => JString(other.toString)
-                             }
-                           }
+                              case SoQLText(str)   => JString(str)
+                              case SoQLNumber(bd)  => JNumber(bd)
+                              case SoQLMoney(bd)   => JNumber(bd)
+                              case SoQLDouble(dbl) => JNumber(dbl)
+                              case SoQLBoolean(b)  => JBoolean(b)
+                              case SoQLFixedTimestamp(dt) => JString(SoQLFixedTimestamp.StringRep(dt))
+                              case SoQLFloatingTimestamp(dt) => JString(SoQLFloatingTimestamp.StringRep(dt))
+                              case SoQLTime(dt) => JString(SoQLTime.StringRep(dt))
+                              case SoQLDate(dt) => JString(SoQLDate.StringRep(dt))
+                              case other: SoQLValue => JString(other.toString)
+                            }
+            }
           }.toMap
           FeatureJson(props, geom)
         }
@@ -270,6 +271,7 @@ object TileService {
       case _: NullPointerException =>        Failure(InvalidSoqlPackException(Map.empty))
     }
   }
+  // scalastyle:on cyclomatic.complexity
 
   private[services] def augmentParams(req: HttpRequest,
                                       where: String,
