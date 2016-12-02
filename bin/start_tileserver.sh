946e736dc912ed9c3a331c2c5b4c0fcea0145392
Kaida Masaki <kaida.masaki@socrata.com>
2016-12-02 10:04:22 -0800

    Add bin/start_tileserver.sh

---
+++ bin/start_tileserver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Start TileServer
+set -e
+
+REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
+BINDIR=$(dirname "$REALPATH")
+
+JARFILE=$("$BINDIR"/build.sh "$@")
+
+java -Djava.net.preferIPv4Stack=true -jar "$JARFILE"
